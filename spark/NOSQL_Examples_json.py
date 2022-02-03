ad977e38891823aacfe6efb8bbe2bd978dd1ed9a
vinit <vinitk0408@gmail.com>
2022-02-03 09:06:17 +0800

    commit

--- spark/NOSQL_Examples_json.py
+++ spark/NOSQL_Examples_json.py
@@ -3,14 +3,29 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+
+# Question Part c
+# JSON files
+# 7. Read data from the JSON file Producers.JSON and display all records.
+# 8. Modify the above query to display those producers who are located in UK.
+
+
+
 def main():
     cnfg = SparkConf().setAppName("CustomerApplication").setMaster("local[2]")
     sc = SparkContext(conf=cnfg)
     spark = SparkSession(sc)
     inputFilePath = "D:\\Workspace\\pthonTest\\pthonTest\\spark\\BEAD_DATA-master\\BEAD_DATA-master\\Producers.json"
     df = (spark.read.option("header", "true").option("inferSchema", "true").json(inputFilePath))
-    df.show(50, False)
-    df.printSchema()
+
+    # 7. Read data from the JSON file Producers.JSON and display all records.
+    df.show(200, False)
+    #df.printSchema()
+
+    # 8. Modify the above query to display those producers who are located in UK.
+    df.filter("Location = 'UK'").show()
+
+   # df.write.csv("D:\\Workspace\\pthonTest\\pthonTest\\spark\\BEAD_DATA-master\\log\\json")
 
 
 if __name__ == '__main__':
---
+++ spark/NOSQL_Execise_MultipleJoin.py
@@ -0,0 +1,26 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+#Question Retrieve all movies produced by Walt Disney Studios. You should display the Producer Name, Location, Movie Title and Movie Type.
+# (Note: The first two fields are from Producer.JSON and the other two fields are from Movies.CSV.
+# The join field is ProcuderCode field. If you have issues, you may change ProducerID column in the CSV to same as in JSON, viz. ProducerCode)
+
+
+def main():
+    cnfg = SparkConf().setAppName("CustomerApplication").setMaster("local[2]")
+    sc = SparkContext(conf=cnfg)
+    spark = SparkSession(sc)
+    inputFilePathProducer = "D:\\Workspace\\pthonTest\\pthonTest\\spark\\BEAD_DATA-master\\BEAD_DATA-master\\Producers.json"
+    dfProducer = (spark.read.option("header", "true").option("inferSchema", "true").json(inputFilePathProducer))
+
+    inputFilePathMovies = "D:\\Workspace\\pthonTest\\pthonTest\\spark\\BEAD_DATA-master\\BEAD_DATA-master\\\MoviesV2.csv"
+    dfMovies = (spark.read.option("header", "true").option("inferSchema", "true").csv(inputFilePathMovies))
+
+    joinDF = dfProducer.join(dfMovies,"ProducerCode").show()
+    joinDF.show("ProducerName","Location","MovieTitle","MovieType").show()
+
+if __name__ == '__main__':
+    main()
