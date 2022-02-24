6f8a61573b33ff608bb42ff4c3e8dbed13e0faca
vinit <vinitk0408@gmail.com>
2022-02-24 08:51:43 +0800

    commit

--- spark/machineLearning/sentimentalAnalysis/vadermoviereview.py
+++ spark/machineLearning/sentimentalAnalysis/vadermoviereview.py
@@ -1,46 +1,32 @@
 from nltk.sentiment import vader
 import nltk
 
-# nltk.download()
+positiveReviewsFileName = "D:\\DataSets\\rt-polaritydata\\rt-polarity.pos"
+with open(positiveReviewsFileName, 'r') as f:
+    positiveReviews = f.readlines()
+    # print(positiveReviews)
+
+negativeReviewsFileName = "D:\\DataSets\\rt-polaritydata\\rt-polarity.neg"
+with open(negativeReviewsFileName, 'r') as f:
+    negativeReviews = f.readlines()
+    # print(negativeReviews)
+
 sia = vader.SentimentIntensityAnalyzer()
-print(sia.polarity_scores("What a terrible restaurant"))
-print()
 
-# Emoticons
-print("Emoticons")
-print(":D : ", sia.polarity_scores(":D"))
-print(":) : ", sia.polarity_scores(":)"))
-print(":/: ", sia.polarity_scores(":/"))
-print()
 
-# Idioms
-print("Idioms")
-print("the cumin was the kiss of death: ", sia.polarity_scores("the cumin was the kiss of death"))
-print()
+def vadeSentiment(review):
+    return sia.polarity_scores(review)['compound']
 
-# Punctuation
-print("Punctuation")
-print("the food was good", sia.polarity_scores("the cumin was the kiss of death"))
-print("Terrible", sia.polarity_scores("Terrible"))
-print("Terrible! : ", sia.polarity_scores("Terrible!"))
-print("Terrible!! : ", sia.polarity_scores("Terrible!!"))
-print()
 
-# Negation
-print("Negation")
-print("the food was good!! :", sia.polarity_scores("the food was good!!"))
-print("the food was not good!! :", sia.polarity_scores("the food was not good!!"))
-print("the food was not the worst!! :", sia.polarity_scores("the food was not the worst!!"))
-print()
+def getReviewSentiments(sentimentCalulator):
+    negReviewResult = [sentimentCalulator(oneNegativeReview) for oneNegativeReview in negativeReviews]
+    posReviewResult = [sentimentCalulator(onePositiveReview) for onePositiveReview in positiveReviews]
+    return {'results-on-positive': posReviewResult, 'results-on-negative': negReviewResult}
 
-# Emphasis like Captilizationa,booster word
-print("Emphasis")
-print("the food was good :", sia.polarity_scores("the food was good"))
-print("the food was GOOD :", sia.polarity_scores("the food was GOOD"))
-print("the food was so good :", sia.polarity_scores("the food was so good"))
-print()
 
-# Contrast
-print("Contrast")
-print("I usually hate seafood but I liked this :", sia.polarity_scores("I usually hate seafood but I liked this"))
-print("I usually hate seafood and I liked this :", sia.polarity_scores("I usually hate seafood and I liked this"))
+review = "This is the best restaurant in the city"
+print(getReviewSentiments(vadeSentiment))
+vaderResults = getReviewSentiments(vadeSentiment)
+print("keys", vaderResults.keys())
+print("positive Reviews", len(vaderResults['results-on-positive']))
+print("negative Reviews", len(vaderResults['results-on-negative']))
