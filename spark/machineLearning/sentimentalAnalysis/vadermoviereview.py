c953cf943ea74f0583059e8170491e7ea8bfdeba
vinit <vinitk0408@gmail.com>
2022-02-24 21:33:01 +0800

    commit

--- spark/machineLearning/sentimentalAnalysis/vadermoviereview.py
+++ spark/machineLearning/sentimentalAnalysis/vadermoviereview.py
@@ -24,9 +24,23 @@ def getReviewSentiments(sentimentCalulator):
     return {'results-on-positive': posReviewResult, 'results-on-negative': negReviewResult}
 
 
+def runDiagnopstics(reviewResult):
+    positiveResult = reviewResult['results-on-positive']
+    negativeResult = reviewResult['results-on-negative']
+    pctTruePositive = float(sum(x > 0 for x in positiveResult)) / len(positiveResult)
+    pctTrueNegative = float(sum(x > 0 for x in negativeResult)) / len(negativeResult)
+
+    totalAccurate = float(sum(x > 0 for x in positiveResult)) + float(sum(x > 0 for x in negativeResult))
+    total = len(positiveResult) + len(negativeResult)
+    print("Accuracy on positive reviews = " + "%.2f" % (pctTruePositive * 100) + "%")
+    print("Accuracy on negative reviews = " + "%.2f" % (pctTrueNegative * 100) + "%")
+    print("Overall accuracy  = " + "%.2f" % (totalAccurate*100/total) + "%")
+
+
 review = "This is the best restaurant in the city"
 print(getReviewSentiments(vadeSentiment))
 vaderResults = getReviewSentiments(vadeSentiment)
 print("keys", vaderResults.keys())
 print("positive Reviews", len(vaderResults['results-on-positive']))
 print("negative Reviews", len(vaderResults['results-on-negative']))
+print(runDiagnopstics(getReviewSentiments(vadeSentiment)))
