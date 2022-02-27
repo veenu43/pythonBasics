564be9cef64ff85bab46f43f4be2dcfdf81c726a
vinit <vinitk0408@gmail.com>
2022-02-27 13:33:03 +0800

    commit

--- spark/machineLearning/sentimentalAnalysis/mlbasedSentimentAnalysis.py
+++ spark/machineLearning/sentimentalAnalysis/mlbasedSentimentAnalysis.py
@@ -1,5 +1,3 @@
-from nltk.corpus import sentiwordnet as swn
-from nltk.sentiment import vader
 from string import punctuation
 from nltk.corpus import stopwords
 import nltk
@@ -23,6 +21,7 @@ testPositiveReviews = positiveReviews[testTrainingSplitIndex + 1:]
 trainingNegativeReviews = negativeReviews[:testTrainingSplitIndex]
 trainingPositiveReviews = positiveReviews[:testTrainingSplitIndex]
 
+
 def getVocabulary():
     positiveWordList = [word for line in trainingPositiveReviews for word in line.split()]
     negativeWordList = [word for line in trainingNegativeReviews for word in line.split()]
@@ -37,52 +36,68 @@ def getVocabulary():
     print("Sample vocabulary: ", vocabulary[0])
     return vocabulary
 
+
 vocabulary = getVocabulary()
 
 
 def getTrainingData():
-    negTaggedTrainingReviewList = [{'review': oneReview.split(),'label': 'negative'} for oneReview in trainingNegativeReviews]
-    posTaggedTrainingReviewList = [{'review': oneReview.split(),'label': 'positive'} for oneReview in trainingPositiveReviews]
-    fullTaggedTrainingReviewList = [item for sublist in [negTaggedTrainingReviewList,posTaggedTrainingReviewList] for item in sublist]
-    trainingData = [(review['review'],review['label']) for review in fullTaggedTrainingReviewList]
-    #print(training)
+    negTaggedTrainingReviewList = [{'review': oneReview.split(), 'label': 'negative'} for oneReview in
+                                   trainingNegativeReviews]
+    print("negTaggedTrainingReviewList: ", len(negTaggedTrainingReviewList))
+    print("Sample negTaggedTrainingReviewList: ", list(negTaggedTrainingReviewList)[0])
+    posTaggedTrainingReviewList = [{'review': oneReview.split(), 'label': 'positive'} for oneReview in
+                                   trainingPositiveReviews]
+    print("posTaggedTrainingReviewList: ", len(posTaggedTrainingReviewList))
+    print("Sample posTaggedTrainingReviewList[0][0]: ", list(posTaggedTrainingReviewList)[0])
+    fullTaggedTrainingReviewList = [item for sublist in [negTaggedTrainingReviewList, posTaggedTrainingReviewList] for
+                                    item in sublist]
+    trainingData = [(review['review'], review['label']) for review in fullTaggedTrainingReviewList]
+    # print(training)
     print("TrainingData: ", len(trainingData))
-    print("Sample TrainingData: ", trainingData[0])
-    return  trainingData
+    print("Sample TrainingData[0][0]: ", trainingData[0][0])
+    print("Sample TrainingData[0][1]: ", trainingData[0][1])
+    return trainingData
+
 
 trainingData = getTrainingData()
+
 
 def extract_features(review):
     review_words = set(review)
     features = {}
     for word in vocabulary:
         features[word] = (word in review_words)
-    #print("Feature sample", features)
+    # print("Feature sample", features)
     return features
 
-def getTrainedNaiveBayesClassifier(extract_features,trainingData):
-    trainingFeatures = nltk.classify.apply_features(extract_features,trainingData)
-    #print(trainingFeatures)
+
+def getTrainedNaiveBayesClassifier(extract_features, trainingData):
+    trainingFeatures = nltk.classify.apply_features(extract_features, trainingData)
+    # print(trainingFeatures)
     trainedNBCClassifier = nltk.NaiveBayesClassifier.train(trainingFeatures)
     return trainedNBCClassifier
 
-trainedNBCClassifier = getTrainedNaiveBayesClassifier(extract_features,trainingData);
+
+trainedNBCClassifier = getTrainedNaiveBayesClassifier(extract_features, trainingData)
+
 
 def naiveBayessSentimentCalculator(review):
     problemInstance = review.split()
     problemFeatures = extract_features(problemInstance)
     return trainedNBCClassifier.classify(problemFeatures)
 
-print("What an Awesome Moview",naiveBayessSentimentCalculator("What an Awesome Movie"))
-print("What an terrible Moview",naiveBayessSentimentCalculator("What an terrible Movie"))
+
+print("What an Awesome Movie", naiveBayessSentimentCalculator("What an Awesome Movie"))
+print("What an terrible Movie", naiveBayessSentimentCalculator("What an terrible Movie"))
+
 
 def getTestReviewSentiments(naiveBayesSentimentCalculator):
     testNegResults = [naiveBayesSentimentCalculator(review) for review in testNegativeReviews]
     testPosResults = [naiveBayesSentimentCalculator(review) for review in testPositiveReviews]
-    lableToNum = {'positive':1,'negative': -1}
+    lableToNum = {'positive': 1, 'negative': -1}
     numericNegResults = [lableToNum[x] for x in testNegResults]
     numericPosResults = [lableToNum[x] for x in testPosResults]
-    return {'results-on-positive':numericPosResults,'results-on-negative':numericNegResults}
+    return {'results-on-positive': numericPosResults, 'results-on-negative': numericNegResults}
 
 
 def runDiagnopstics(reviewResult):
@@ -95,6 +110,7 @@ def runDiagnopstics(reviewResult):
     total = len(positiveResult) + len(negativeResult)
     print("Accuracy on positive reviews = " + "%.2f" % (pctTruePositive * 100) + "%")
     print("Accuracy on negative reviews = " + "%.2f" % (pctTrueNegative * 100) + "%")
-    print("Overall accuracy  = " + "%.2f" % (totalAccurate*100/total) + "%")
+    print("Overall accuracy  = " + "%.2f" % (totalAccurate * 100 / total) + "%")
+
 
 runDiagnopstics(getTestReviewSentiments(naiveBayessSentimentCalculator))
